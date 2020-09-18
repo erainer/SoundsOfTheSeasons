85b33f7c79da6577254b3d5994cc343c3be89cbe
Emily Rainer <emily.rainer@lextech.com>
2020-09-18 07:50:24 -0500

    Navigation setup.

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+//import { Audio } from 'expo-av';
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button 
+        title="GoToHalloween" 
+        onPress={() => navigation.navigate("Halloween")}
+      />
+    </View>
+  )
+}
+
+function HalloweenScreen() {
+  return (
+    <View>
+      <Text>Halloween</Text>
+      <Button 
+        title="Child Whispers" 
+        // onPress={() => navigation.navigate("Halloween")}
+      />
+    </View>
+  )
+}
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Halloween" component={HalloweenScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
